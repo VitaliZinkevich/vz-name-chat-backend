84080bbdaf16823aa3f4aebec865b90b79995fa3
Vitali Zinkevich <vitalizinkevich@gmail.com>
2020-02-02 16:32:45 +0300

    чат с почтой

--- handler.js
+++ handler.js
@@ -1,41 +1,124 @@
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 let dynamo = new AWS.DynamoDB.DocumentClient();
 
-require('aws-sdk/clients/apigatewaymanagementapi'); 
+require("aws-sdk/clients/apigatewaymanagementapi");
 
-const CHATCONNECTION_TABLE = 'chatIdTable';
+const CHATCONNECTION_TABLE = "chatIdTable";
 
 const successfullResponse = {
   statusCode: 200,
-  body: 'everything is alright'
+  body: "everything is alright"
 };
 
 module.exports.connectionHandler = (event, context, callback) => {
-  console.log(event);
+  let email = "vitalizinkevich@gmail.com";
+  const textBody = `
+    текст тела письма
+  `;
 
-  if (event.requestContext.eventType === 'CONNECT') {
+  if (event.requestContext.eventType === "CONNECT") {
     // Handle connection
     addConnection(event.requestContext.connectionId)
       .then(() => {
-        callback(null, successfullResponse);
+        const paramsAdmin = {
+          Destination: {
+            ToAddresses: [email]
+          },
+          Message: {
+            Body: {
+              Html: {
+                Charset: "UTF-8",
+                Data: `КТО ТО ПОДКЛЮЧЕН`
+              },
+              Text: {
+                Charset: "UTF-8",
+                Data: textBody
+              }
+            },
+            Subject: {
+              Charset: "UTF-8",
+              Data: "сообщение сайта по web dev"
+            }
+          },
+          Source: "vitalizinkevich@gmail.com"
+        };
+        let emailPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+          .sendEmail(paramsAdmin)
+          .promise();
+
+        emailPromise
+          .then(res => {
+            callback(null, successfullResponse);
+          })
+          .catch(err => {
+            callback(null, {
+              statusCode: 500,
+              headers: {
+                "Content-Type": "text/plain",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true
+              },
+              body: message || "error message"
+            });
+          });
       })
       .catch(err => {
         console.log(err);
         callback(null, JSON.stringify(err));
       });
-  } else if (event.requestContext.eventType === 'DISCONNECT') {
+  } else if (event.requestContext.eventType === "DISCONNECT") {
     // Handle disconnection
     deleteConnection(event.requestContext.connectionId)
       .then(() => {
-        callback(null, successfullResponse);
+        const paramsAdmin = {
+          Destination: {
+            ToAddresses: [email]
+          },
+          Message: {
+            Body: {
+              Html: {
+                Charset: "UTF-8",
+                Data: `КТО ТО ОТКЛЮЧИЛСЯ`
+              },
+              Text: {
+                Charset: "UTF-8",
+                Data: textBody
+              }
+            },
+            Subject: {
+              Charset: "UTF-8",
+              Data: "сообщение сайта по web dev"
+            }
+          },
+          Source: "vitalizinkevich@gmail.com"
+        };
+        let emailPromise = new AWS.SES({ apiVersion: "2010-12-01" })
+          .sendEmail(paramsAdmin)
+          .promise();
+
+        emailPromise
+          .then(res => {
+            callback(null, successfullResponse);
+          })
+          .catch(err => {
+            callback(null, {
+              statusCode: 500,
+              headers: {
+                "Content-Type": "text/plain",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true
+              },
+              body: message || "error message"
+            });
+          });
       })
       .catch(err => {
         console.log(err);
         callback(null, {
           statusCode: 500,
-          body: 'Failed to connect: ' + JSON.stringify(err)
+          body: "Failed to connect: " + JSON.stringify(err)
         });
       });
   }
@@ -43,45 +126,48 @@ module.exports.connectionHandler = (event, context, callback) => {
 
 // THIS ONE DOESNT DO ANYHTING
 module.exports.defaultHandler = (event, context, callback) => {
-  console.log('defaultHandler was called');
+  console.log("defaultHandler was called");
   console.log(event);
 
   callback(null, {
     statusCode: 200,
-    body: 'defaultHandler'
+    body: "defaultHandler"
   });
 };
 
 module.exports.sendMessageHandler = (event, context, callback) => {
-  sendMessageToAllConnected(event).then(() => {
-    callback(null, successfullResponse)
-  }).catch (err => {
-    callback(null, JSON.stringify(err));
-  });
-}
+  sendMessageToAllConnected(event)
+    .then(() => {
+      callback(null, successfullResponse);
+    })
+    .catch(err => {
+      callback(null, JSON.stringify(err));
+    });
+};
 
-const sendMessageToAllConnected = (event) => {
+const sendMessageToAllConnected = event => {
   return getConnectionIds().then(connectionData => {
     return connectionData.Items.map(connectionId => {
       return send(event, connectionId.connectionId);
     });
   });
-}
+};
 
-const getConnectionIds = () => {  
+const getConnectionIds = () => {
   const params = {
     TableName: CHATCONNECTION_TABLE,
-    ProjectionExpression: 'connectionId'
+    ProjectionExpression: "connectionId"
   };
 
   return dynamo.scan(params).promise();
-}
+};
 
 const send = (event, connectionId) => {
   const body = JSON.parse(event.body);
-  const postData = body.data;  
+  const postData = body.data;
 
-  const endpoint = event.requestContext.domainName + "/" + event.requestContext.stage;
+  const endpoint =
+    event.requestContext.domainName + "/" + event.requestContext.stage;
   const apigwManagementApi = new AWS.ApiGatewayManagementApi({
     apiVersion: "2018-11-29",
     endpoint: endpoint
@@ -98,7 +184,7 @@ const addConnection = connectionId => {
   const params = {
     TableName: CHATCONNECTION_TABLE,
     Item: {
-      connectionId: connectionId 
+      connectionId: connectionId
     }
   };
 
@@ -109,7 +195,7 @@ const deleteConnection = connectionId => {
   const params = {
     TableName: CHATCONNECTION_TABLE,
     Key: {
-      connectionId: connectionId 
+      connectionId: connectionId
     }
   };
 
